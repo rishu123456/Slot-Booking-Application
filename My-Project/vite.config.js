72b100900545fd5fd4e3e0f6e9cdb5e526a26e9b
rishu123456 <rishu25112001@gamil.com>
2025-02-15 16:43:18 +0530

    deployment-fix

--- My-Project/vite.config.js
+++ My-Project/vite.config.js
@@ -5,10 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
-  ],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'], 
-    },
-  },
+  ]
 })
